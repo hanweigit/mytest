590e231c03ae6cce265ed2706abd02499660e954
hanweigit <2411456113@qq.com>
2021-05-25 21:58:18 +0800

    add MyTest

---
+++ src/main/java/MyTest.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+/**
+ * @Description :
+ * @Author : HanWei
+ * @Time : 2021/5/25 19:36
+ * @UpdateRemark:
+ * @UpdateDate:2021/5/25 19:36
+ * @Version:1.0
+ **/
+public class MyTest {
+    private static String str;
+    private static int member;
+    public String method(String str,int member){
+
+        /***
+        *
+        *@Description:
+        *@Param:[str, member]
+        *@Return:java.lang.String
+        *@Author:HanWei
+        *@time:2021/5/25 19:38
+        *
+        ***/
+        Random random=new Random();
+        return "hello are you ok";
+
+    }
+}
